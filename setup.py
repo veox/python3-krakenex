83911551e839f8c37e9a9c13776dd0f2dea3b1a9
Noel Maersk <veox@wemakethings.net>
2013-10-27 21:58:28 +0200

    Make it into a package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name='krakenex',
+      version='0.1',
+      description='kraken.com cryptocurrency exchange API',
+      author='Noel Maersk',
+      author_email='veox@wemakethings.net',
+      url='https://github.com/veox/KrakenExchangeAPI',
+      packages=['krakenex'],
+     )
