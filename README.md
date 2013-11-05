bdba4e249262768ae5c91ef1ef59dc4732448d2d
Noel Maersk <veox@wemakethings.net>
2013-11-05 14:47:18 +0200

    Change license to GPLv3.

--- README.md
+++ README.md
@@ -22,13 +22,14 @@ For a list of public/private API methods, see
 Attribution
 -----------
 
-This code is licensed under the ISC license. It should be available in
-`LICENSE`.
+This code is licensed under the GPLv3 license. It should be available in
+`LICENSE`. If not, see [here][gnugpl].
 
 Payward's [PHP API][krakenphpapi] and ScriptProdigy's [Cryptsy Python
 API][cryptsypyapi] were used as examples.
 
 
+[gnugpl]: https://www.gnu.org/licenses/gpl-3.0.txt
 [krakenphpapi]: https://github.com/payward/kraken-api-client
 [cryptsypyapi]: https://github.com/ScriptProdigy/CryptsyPythonAPI
 [krakenapidoc]: https://www.kraken.com/help/api
--- krakenex/api.py
+++ krakenex/api.py
@@ -1,18 +1,19 @@
-# Copyright (c) 2013, Noel Maersk <veox tat wemakethings dotnet>
+# This file is part of krakenex.
 #
-# Permission to use, copy, modify, and/or distribute this software for
-# any purpose with or without fee is hereby granted, provided that
-# the above copyright notice and this permission notice appear in all
-# copies.
+# krakenex is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
-# WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
-# AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
-# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
-# OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
-# TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-# PERFORMANCE OF THIS SOFTWARE.
+# krakenex is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General
+# Public LICENSE along with krakenex. If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.txt>.
+
 
 import json
 import urllib
