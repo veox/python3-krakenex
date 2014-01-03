9fd942b3d241b9a188633c1a54f7777d6cc4217d
Noel Maersk <veox@wemakethings.net>
2014-01-04 01:54:42 +0200

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='krakenex',
-      version='0.0.1',
+      version='0.0.2',
       description='kraken.com cryptocurrency exchange API',
       author='Noel Maersk',
       author_email='veox@wemakethings.net',
