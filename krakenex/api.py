308a7084f3b59326c6f844e158de5e57ee34f322
Noel Maersk <veox@wemakethings.net>
2014-08-12 10:51:51 +0300

    misc: changed license headers in source files, too.

--- krakenex/api.py
+++ krakenex/api.py
@@ -1,17 +1,17 @@
 # This file is part of krakenex.
 #
 # krakenex is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version.
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # krakenex is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General
-# Public LICENSE along with krakenex. If not, see
+# You should have received a copy of the GNU Lesser
+# General Public LICENSE along with krakenex. If not, see
 # <http://www.gnu.org/licenses/gpl-3.0.txt>.
 
 
