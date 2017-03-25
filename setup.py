e84d7a879c96946936973696da46ea3cde1d11e1
Noel Maersk <veox@users.noreply.github.com>
2017-03-25 20:40:50 +0200

    doc/setup: use README.rst for long_description.

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ from distutils.core import setup
 
 exec(open('./krakenex/version.py').read())
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name='krakenex',
       version=__version__,
       description='kraken.com cryptocurrency exchange API',
+      long_description=read('README.rst'),
       author='Noel Maersk',
       author_email='veox+packages+spamremove@veox.pw',
       url=__url__,
