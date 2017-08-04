5dbcaeb6546878b089325c511f0fb093bd6f17ed
Noel Maersk <veox@users.noreply.github.com>
2017-08-04 16:03:14 +0300

    examples: clarify what fields are valid for "conditional close" orders.

--- examples/README.rst
+++ examples/README.rst
@@ -3,9 +3,12 @@ krakenex examples
 
 Here are some simple examples of how ``krakenex`` can be used.
 
-They are not maintained to the same standard as the "core" package,
+They are not maintained to the same standard as the package itself,
 and should be viewed as best-effort contributions by their respective
 authors.
+
+Be sure to understand what a script does before you run it with a valid
+``kraken.key`` file!
 
 
 License
--- examples/order-conditional-close.py
+++ examples/order-conditional-close.py
@@ -8,19 +8,25 @@
 #
 # This can be useful for very simple automation, where a bot is not
 # needed to constantly monitor execution.
+#
+# Complex nesting seems to not be allowed by Kraken. See:
+# https://github.com/veox/python3-krakenex/issues/39
 
 import krakenex
 
 k = krakenex.API()
 k.load_key('kraken.key')
 
-k.query_private('AddOrder', {'pair': 'XXBTZEUR',
-                             'type': 'buy',
-                             'ordertype': 'limit',
-                             'price': '1',
-                             'volume': '1',
-                             'close[pair]': 'XXBTZEUR',
-                             'close[type]': 'sell',
-                             'close[ordertype]': 'limit',
-                             'close[price]': '9001',
-                             'close[volume]': '1'})
+k.query_private('AddOrder',
+                {'pair': 'XXBTZEUR',
+                 'type': 'buy',
+                 'ordertype': 'limit',
+                 'price': '1',
+                 'volume': '1',
+                 # only `ordertype`, `price` and `price2` are valid
+                 'close[ordertype]': 'limit',
+                 'close[price]': '9001',
+                 # these will be ignored!
+                 'close[pair]': 'XXBTZEUR',
+                 'close[type]': 'sell',
+                 'close[volume]': '1'})
